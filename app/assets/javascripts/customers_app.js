8d3240903a2b1788185f4b7f9a461c315382b1d0
C.R. Myers <myers3.cr@gmail.com>
2015-09-21 18:26:25 -0400

    angularized search with canned return data

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,10 +1,34 @@
 var app = angular.module('customers',[]); 
 
-app.controller("CustomerSearchController", [
-          '$scope',
-  function($scope) {
-    $scope.search = function(searchTerm) {
-      $scope.searchedFor = searchTerm;
+app.controller("CustomerSearchController", [ 
+          '$scope',                          
+  function($scope) {                         
+    $scope.customers = [];
+    $scope.search = function(searchTerm) {   
+      $scope.customers = [
+        {
+          "first_name" : "C.R.",
+          "last_name" : "Myers",
+          "email" : "myers3.cr@gmail.com",
+          "username" : "myers3cr",
+          "created_at" : "2015-06-12",
+        },
+        {
+          "first_name" : "Joanne",
+          "last_name" : "Redmore",
+          "email" : "jredmore4@gmail.com",
+          "username" : "jsr",
+          "created_at" : "2015-12-04",
+        },
+        {
+          "first_name" : "Harvey",
+          "last_name" : "Foonman",
+          "email" : "hxf@example.com",
+          "username" : "hfoonman",
+          "created_at" : "2015-07-04",
+        }
+      ]
+      
     }
   }
 ]);
